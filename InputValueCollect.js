d5a4b3e82edf333a4f114b2b63066af73d8370c0
suvo <suvodatta72@gmail.com>
2024-08-29 16:32:03 +0600

    inupt value display by Javascript

---
+++ InputValueCollect.js
@@ -0,0 +1,10 @@
+const inputValue = document.getElementById("myInput");
+const btn1 = document.getElementById("btn1");
+const outPut = document.getElementById("output")
+
+
+
+
+btn1.addEventListener("click", () =>{
+      return outPut.innerHTML = inputValue.value
+})
